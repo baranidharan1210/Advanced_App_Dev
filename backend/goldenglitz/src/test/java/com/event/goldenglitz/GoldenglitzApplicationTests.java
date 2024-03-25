e8e3b52b047913ea094e0a4563591d44b5d05742
Barani18dharan <baranidharan1818@gmail.com>
2024-03-25 18:48:03 +0530

    backend

---
+++ backend/goldenglitz/src/test/java/com/event/goldenglitz/GoldenglitzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.event.goldenglitz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldenglitzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
